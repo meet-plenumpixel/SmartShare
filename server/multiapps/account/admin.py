566d4ceff7e3c39cea9af46b79bd1dec8fffba84
MeetPlenumPixel <meet.plenumpixel@gmail.com>
2022-03-25 16:32:15 +0530

    create UserExpense and UserGroup model

--- server/multiapps/account/admin.py
+++ server/multiapps/account/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from account.models import UserAccount
+from account.models import UserAccount, UserExpense, UserGroup
 # Register your models here.
 
 
 @admin.register(UserAccount)
 class UserAccountAdmin(UserAdmin):
-  model = UserAccount
+  search_fields = ('username','email')
 
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.fieldsets[1][1]['fields'] += ("balance", "image")
-  
+
+
+@admin.register(UserExpense)
+class UserExpenseAdmin(admin.ModelAdmin):
+
+  list_display = ('title', 'amount', 'pay_by')
+  search_fields = ('title', 'pay_by')
+  filter_horizontal= ('contributor',)
+
+
+@admin.register(UserGroup)
+class UserGroupAdmin(admin.ModelAdmin):
+
+  list_display = ('name', 'owner')
+  search_fields = ('name', 'owner')
+  filter_horizontal= ('members',)
--- server/multiapps/account/models.py
+++ server/multiapps/account/models.py
@@ -3,9 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class UserAccount(AbstractUser):
   balance = models.DecimalField(default=0, max_digits=15, decimal_places=2)
   image = models.ImageField(default='profile_pics/user_default.jpg', upload_to='profile_pics')
 
   class Meta:
     unique_together = (('username', 'email'),)
+
+
+class UserExpense(models.Model):
+  title = models.CharField(unique=True, max_length=50)
+  detail = models.TextField(blank=True, max_length=200)
+  amount = models.DecimalField(max_digits=6, decimal_places=2)
+  pay_by = models.ForeignKey('UserAccount', related_name='paid_expenses', on_delete=models.PROTECT)
+  contributor = models.ManyToManyField('UserAccount', related_name='expense_contributor')
+
+
+class UserGroup(models.Model):
+  name = models.CharField(unique=True, max_length=30)
+  owner = models.ForeignKey('UserAccount', related_name='my_groups', on_delete=models.PROTECT)
+  members = models.ManyToManyField('UserAccount')
