462a29119f3bcf7a03405ee51336aeb09bb5e0eb
MeetPlenumPixel <meet.plenumpixel@gmail.com>
2022-03-26 17:31:46 +0530

    add dropdown menu to nav bar

--- server/multiapps/home/urls.py
+++ server/multiapps/home/urls.py
@@ -6,5 +6,5 @@ from account import views as user_views
 urlpatterns = [
   path('', home_views.HomeTemplateView.as_view(), name='home'),
   path('profile/', home_views.UserUpdateView.as_view(), name='profile_detail'),
-  # path('profile-list/', user_views.UserListView.as_view(), name='profile_list'),
+  path('create-gp/', home_views.UserUpdateView.as_view(), name='create_gp'),
 ]
