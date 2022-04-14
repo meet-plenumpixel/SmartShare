6612488d1d6fb375da6a92cf76a3167168765f79
MeetPlenumPixel <meet.plenumpixel@gmail.com>
2022-04-14 21:47:14 +0530

    comment static dir from gitignore file

---
+++ server/multiapps/contrib/static/widgets/js/checkbox_select_multiple.js
@@ -0,0 +1,41 @@
+// (function ($) {
+//   'use strict';
+
+  $(document).ready(function () {
+
+    console.log('working!');
+    $('.checkboc_select_multiple')
+      .on('click', '.dropdown-button', function () {
+        $(this).siblings('.dropdown-list').toggle(function(){
+          if ($(this).is(':visible')) {
+            $(this).children(".dropdown-search:first").focus();
+          }
+        });
+      })
+      .on('input', '.dropdown-search', function () {
+        var target = $(this);
+        var dropdownList = target.closest('.dropdown-list');
+        var search = target.val().toLowerCase();
+
+
+        if (!search) {
+          dropdownList.find('li').show();
+          return false;
+        }
+
+        dropdownList.find('li').each(function () {
+          var text = $(this).text().toLowerCase();
+          var match = text.indexOf(search) > -1;
+          $(this).toggle(match);
+        });
+      })
+      .on('change', '[type="checkbox"]', function () {
+        var container = $(this).closest('.checkboc_select_multiple');
+        var numChecked = container.find('[type="checkbox"]:checked').length;
+        container.find('.quantity').text(numChecked || 'Any');
+      });
+
+  })
+
+// })(jQuery);
+// }(jQuery) )
